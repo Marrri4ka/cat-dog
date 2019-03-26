0f4da154a1d57a70a25c6ad7f9e16d76d44977cb
Mariia Stashuk and Zema Kebede <mariia@>
2019-03-26 13:20:26 -0700

    Apply different background colors if clicking bark vs meow

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
-
-    $('.cat').css('background-color', 'green');
-    $('.dog').css('background-color', 'blue');
     $(".cat").prepend("<li>Meow!</li>");
     $(".dog").prepend("<li>Bark!</li>");
+
+    $('.cat').children('li').first().css('background-color', 'green');
+    $('.dog').children('li').first().css('background-color', 'blue');
+
+
+
 
   });
 
   $("button#bark").click(function() {
-    
-    $('.cat').css('background-color', 'pink');
-    $('.dog').css('background-color', 'yellow');
     $(".cat").prepend("<li>Meow!</li>");
     $(".dog").prepend("<li>Bark!</li>");
+    $('.cat').children('li').first().css('background-color', 'pink');
+    $('.dog').children('li').first().css('background-color', 'yellow');
+
+
+
+
   });
 
 });
